eb36a8b342c5779e64462876fbe97696591bed15
AnuPriya2131 <45963290+AnuPriya2131@users.noreply.github.com>
2019-01-04 12:14:59 +0530

    Add files via upload
    Project 2 DEMO

---
+++ DNSZone.ps1
@@ -0,0 +1,5 @@
+﻿Login-AzAccount
+
+
+New-AzDnsZone -Name "globantanupriya.tk" -ResourceGroupName "Resgrp"
+New-AzDnsRecordSet -Name "www" -ResourceGroupName "Resgrp" -ZoneName "globantanupriya.tk" -RecordType "A" -Ttl "3600" -DnsRecords (New-AzDnsRecordConfig -Ipv4Address "168.62.179.132")
